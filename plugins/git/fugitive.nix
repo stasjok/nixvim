006192428db84620ebdfd605dd05772d727da646
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2024-07-16 15:10:23 +0300

    plugins/fugitive: add compatibility with combinePlugins and mini.nvim

--- plugins/git/fugitive.nix
+++ plugins/git/fugitive.nix
@@ -13,5 +13,12 @@ helpers.vim-plugin.mkVimPlugin config {
 
   maintainers = [ lib.maintainers.GaetanLepage ];
 
+  extraConfig = cfg: {
+    # mini.nvim and fugitive have duplicate doc tags
+    performance.combinePlugins.standalonePlugins = lib.mkIf (
+      config.performance.combinePlugins.enable && config.plugins.mini.enable
+    ) [ cfg.package ];
+  };
+
   # In typical tpope fashion, this plugin has no config options
 }
