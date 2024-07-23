b00b4b078aa8571eda91408aba67a48734707469
Stanislav Asunkin <1353637+stasjok@users.noreply.github.com>
2024-07-23 12:45:59 +0300

    Revert "plugins/fugitive: add compatibility with combinePlugins and mini.nvim"
    This reverts commit 006192428db84620ebdfd605dd05772d727da646.

--- plugins/git/fugitive.nix
+++ plugins/git/fugitive.nix
@@ -13,12 +13,5 @@ helpers.vim-plugin.mkVimPlugin config {
 
   maintainers = [ lib.maintainers.GaetanLepage ];
 
-  extraConfig = cfg: {
-    # mini.nvim and fugitive have duplicate doc tags
-    performance.combinePlugins.standalonePlugins = lib.mkIf (
-      config.performance.combinePlugins.enable && config.plugins.mini.enable
-    ) [ cfg.package ];
-  };
-
   # In typical tpope fashion, this plugin has no config options
 }
